65296e04ee64b92d50ebabf723f118b48fd10931
John Mileham <jmileham@gmail.com>
2016-11-02 12:58:52 -0400

    Upgrade SAML libs and make the signout button work (#10)
    The signout route previously worked differently with password auth

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
 
     devise_scope :admin do
       get 'admins/sign_in', to: 'admin/sessions#new', as: :new_admin_session
-      get 'admins/sign_out', to: 'devise/sessions#destroy', as: :destroy_admin_session
+      delete 'admins/sign_out', to: 'devise/sessions#destroy', as: :destroy_admin_session
     end
   else
     devise_for :admins
--- spec/features/admin_login_spec.rb
+++ spec/features/admin_login_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'admin login' do
 
   it 'displays active splits to authenticated admin' do
     login
+    expect(subject).to be_displayed
     expect(subject).to have_splits_table
+    subject.log_out.click
+    expect(app.admin_session_new_page).to be_displayed
   end
 end
