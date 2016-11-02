b39271c885f1508fac0f3171c71959c93b5b6732
John Mileham <john@betterment.com>
2016-11-02 12:20:54 -0400

    Upgrade SAML libs and make the signout button work
    The signout route previously worked differently with password auth

--- spec/support/pages/admin/split_index_page.rb
+++ spec/support/pages/admin/split_index_page.rb
@@ -2,4 +2,5 @@ class AdminSplitIndexPage < SitePrism::Page
   set_url '/admin'
 
   element :splits_table, 'body.splits.index table'
+  element :log_out, '.performer-nav a[href="/admins/sign_out"]'
 end
