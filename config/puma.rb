2853e9ed4910853ca4db6ff05840914da0fdbb0d
Sam Moore <sam@betterment.com>
2016-12-20 10:08:59 -0500

    tidy up puma config to work locally

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,17 @@
-worker_count = Integer(ENV['PUMA_WORKER_COUNT'])
-workers worker_count
+rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+environment rails_env
 
-thread_count = Integer(ENV['PUMA_THREAD_COUNT'])
-threads thread_count, thread_count
-
-environment ENV['RAILS_ENV'] || 'development'
-
-bind 'unix:///var/run/puma/testtrack.sock'
-
-daemonize true
-pidfile '/var/run/puma/testtrack.pid'
+if rails_env == 'development'
+  thread_count = Integer(ENV.fetch('PUMA_THREAD_COUNT', '5'))
+  threads thread_count, thread_count
+  port ENV.fetch('PORT', '3000')
+else
+  worker_count = Integer(ENV.fetch('PUMA_WORKER_COUNT'))
+  workers worker_count
+  thread_count = Integer(ENV.fetch('PUMA_THREAD_COUNT'))
+  threads thread_count, thread_count
+  bind 'unix:///var/run/puma/testtrack.sock'
+end
 
 preload_app!
 
