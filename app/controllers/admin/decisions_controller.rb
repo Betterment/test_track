320d57268ded0975b4eab3417190fb5c60c984ae
Sam Moore <sam@betterment.com>
2016-12-22 17:59:35 -0500

    tidying up

--- app/controllers/admin/decisions_controller.rb
+++ app/controllers/admin/decisions_controller.rb
@@ -6,15 +6,15 @@ class Admin::DecisionsController < AuthenticatedAdminController
 
   def create
     split = Split.find params[:split_id]
-    affected_assignments = split.assignments.where.not(variant: target_variant)
+    affected_assignments_count = split.assignments.where.not(variant: target_variant).count
     CreateDecisionJob.perform_later(split, variant: target_variant, admin: current_admin)
-    flash[:success] = "Queued decision to reassign #{affected_assignments.count} visitors to #{target_variant}"
+    flash[:success] = "Queued decision to reassign #{affected_assignments_count} visitors to #{target_variant}"
     redirect_to admin_split_path(split)
   end
 
   private
 
   def target_variant
-    params.require(:decision).fetch(:variant)
+    params.require(:decision).permit(:variant).fetch(:variant)
   end
 end
