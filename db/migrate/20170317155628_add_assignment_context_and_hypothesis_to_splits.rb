59e88def2c4d7c1dfb863f14c31cfc4de12464ea
agirlnamedsophia <sophia.russell@betterment.com>
2017-03-24 16:53:32 -0400

    synopsis to details

--- db/migrate/20170317155628_add_assignment_context_and_hypothesis_to_splits.rb
+++
@@ -1,6 +0,0 @@
-class AddAssignmentContextAndHypothesisToSplits < ActiveRecord::Migration
-  def change
-    add_column :splits, :hypothesis, :text, null: true
-    add_column :splits, :assignment_context, :text, null: true
-  end
-end
