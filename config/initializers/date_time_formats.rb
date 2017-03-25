fe6276d848399601a3b6624e4fd143698471d3c0
agirlnamedsophia <sophia.russell@betterment.com>
2017-03-25 10:23:00 -0400

    adds datetime formats reflected in new designs

---
+++ config/initializers/date_time_formats.rb
@@ -0,0 +1,5 @@
+Date::DATE_FORMATS[:slashed_full_date] = '%-m/%-d/%Y'
+Time::DATE_FORMATS[:slashed_full_date] = '%-m/%-d/%Y'
+
+Date::DATE_FORMATS[:timestamp_in_utc] = '%H:%M %Z'
+Time::DATE_FORMATS[:timestamp_in_utc] = '%H:%M %Z'
