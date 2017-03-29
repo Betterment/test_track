67262152728c44756a97d8d3278cec3928c01c44
agirlnamedsophia <sophia.russell@betterment.com>
2017-03-29 13:52:15 -0400

    clean up use of SUIT and despecify some site prism pages

--- spec/support/pages/admin/split_index_page.rb
+++ spec/support/pages/admin/split_index_page.rb
@@ -2,5 +2,5 @@ class AdminSplitIndexPage < SitePrism::Page
   set_url '/admin'
 
   element :splits_table, 'body.AdminSplits--index table'
-  element :log_out, 'header a[href="/admins/sign_out"]'
+  element :log_out, 'a[href="/admins/sign_out"]'
 end
