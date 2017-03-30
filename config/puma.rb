6cbf64ee81ed65d10a0b4dac2167af055a004204
John Mileham <john@betterment.com>
2017-03-30 07:58:06 -0400

    Don't depend on non-12-factor-config for puma to work

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,20 @@
-rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
-environment rails_env
+workers Integer(ENV['PUMA_WORKER_COUNT'] || ENV['WEB_CONCURRENCY'] || 2)
+thread_count = Integer(ENV['PUMA_THREAD_COUNT'] || ENV['RAILS_MAX_THREADS'] || 5)
+threads thread_count, thread_count
 
-if rails_env == 'development'
-  thread_count = Integer(ENV.fetch('PUMA_THREAD_COUNT', '5'))
-  threads thread_count, thread_count
+preload_app!
+
+rackup      DefaultRackup
+
+rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+
+if rails_env == 'development' || ENV['PORT']
   port ENV.fetch('PORT', '3000')
 else
-  worker_count = Integer(ENV.fetch('PUMA_WORKER_COUNT'))
-  workers worker_count
-  thread_count = Integer(ENV.fetch('PUMA_THREAD_COUNT'))
-  threads thread_count, thread_count
   bind 'unix:///var/run/puma/testtrack.sock'
 end
 
-preload_app!
+environment rails_env
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
