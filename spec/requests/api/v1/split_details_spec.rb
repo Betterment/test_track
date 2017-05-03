18a0e3a1c1668741d9811886cb58d0398ae5d03e
Dan Schaub <dan.s@betterment.com>
2017-05-03 16:07:54 -0400

    Use the paperclip setter for screenshot, and rearrange tests so that happens as infrequently as possible

--- spec/requests/api/v1/split_details_spec.rb
+++ spec/requests/api/v1/split_details_spec.rb
@@ -3,81 +3,87 @@ require 'rails_helper'
 RSpec.describe Api::V1::SplitDetailsController, type: :request do
   describe 'GET /api/v1/split_details/:id' do
     let(:default_app) { FactoryGirl.create :app, name: "default_app", auth_secret: "6Sd6T7T6Q8hKcoo0t8CTzV0IdN1EEHqXB2Ig4raZsOf" }
-    let(:split_with_no_details) { FactoryGirl.create :split, registry: { hammer: 99, nail: 1 }, name: "fantastic_split" }
-    let(:split_with_details) { FactoryGirl.create :split, registry: { enabled: 99, disabled: 1 }, name: "fantastic_split_with_information", platform: 'mobile', description: 'Greatest Split', assignment_criteria: "Must love problem solvers", hypothesis: 'Will solve all problems', location: 'Everywhere', owner: 'Me' } # rubocop:disable Metrics/LineLength
-
-    let!(:variant_detail_a) do
-      FactoryGirl.create(
-        :variant_detail,
-        split: split_with_details,
-        variant: 'enabled',
-        display_name: 'fantastic_split_with_information is on',
-        description: 'This awesome feature makes cool stuff happen.'
-      )
-    end
-    let!(:variant_detail_b) do
-      FactoryGirl.create(
-        :variant_detail,
-        split: split_with_details,
-        variant: 'disabled',
-        display_name: 'fantastic_split_with_information is off',
-        description: 'This feature makes nothing happen.',
-        screenshot_file_name: 'screenshot.jpg'
-      )
-    end
 
     before do
       http_authenticate username: default_app.name, password: default_app.auth_secret
     end
 
-    it "responds with empty details if split has no details" do
-      get "/api/v1/split_details/#{split_with_no_details.name}"
+    context 'with no split details' do
+      let(:split_with_no_details) { FactoryGirl.create :split, registry: { hammer: 99, nail: 1 }, name: "fantastic_split" }
 
-      expect(response_json).to eq(
-        "name" => split_with_no_details.name,
-        "hypothesis" => nil,
-        "location" => nil,
-        "assignment_criteria" => nil,
-        "platform" => nil,
-        "description" => nil,
-        "owner" => nil,
-        "variant_details" => [
-          {
-            "name" => "hammer",
-            "description" => nil
-          },
-          {
-            "name" => "nail",
-            "description" => nil
-          }
-        ]
-      )
+      it "responds with empty details" do
+        get "/api/v1/split_details/#{split_with_no_details.name}"
+
+        expect(response_json).to eq(
+          "name" => split_with_no_details.name,
+          "hypothesis" => nil,
+          "location" => nil,
+          "assignment_criteria" => nil,
+          "platform" => nil,
+          "description" => nil,
+          "owner" => nil,
+          "variant_details" => [
+            {
+              "name" => "hammer",
+              "description" => nil
+            },
+            {
+              "name" => "nail",
+              "description" => nil
+            }
+          ]
+        )
+      end
     end
 
-    it "responds with details if the split has details" do
-      get "/api/v1/split_details/#{split_with_details.name}"
+    context 'with split details' do
+      let(:split_with_details) { FactoryGirl.create :split, registry: { enabled: 99, disabled: 1 }, name: "fantastic_split_with_information", platform: 'mobile', description: 'Greatest Split', assignment_criteria: "Must love problem solvers", hypothesis: 'Will solve all problems', location: 'Everywhere', owner: 'Me' } # rubocop:disable Metrics/LineLength
 
-      expect(response_json).to include(
-        "name" => split_with_details.name,
-        "hypothesis" => split_with_details.hypothesis,
-        "location" => split_with_details.location,
-        "assignment_criteria" => split_with_details.assignment_criteria,
-        "platform" => split_with_details.platform,
-        "description" => split_with_details.description,
-        "owner" => split_with_details.owner
-      )
+      let!(:variant_detail_a) do
+        FactoryGirl.create(
+          :variant_detail,
+          split: split_with_details,
+          variant: 'enabled',
+          display_name: 'fantastic_split_with_information is on',
+          description: 'This awesome feature makes cool stuff happen.'
+        )
+      end
+      let!(:variant_detail_b) do
+        FactoryGirl.create(
+          :variant_detail,
+          split: split_with_details,
+          variant: 'disabled',
+          display_name: 'fantastic_split_with_information is off',
+          description: 'This feature makes nothing happen.',
+          screenshot: File.open(Rails.root.join('spec/support/uploads/ttlogo.png'))
+        )
+      end
 
-      expect(response_json['variant_details'][0]).to eq(
-        "name" => "fantastic_split_with_information is on",
-        "description" => 'This awesome feature makes cool stuff happen.'
-      )
+      it "responds with details" do
+        get "/api/v1/split_details/#{split_with_details.name}"
 
-      expect(response_json['variant_details'][1]).to include(
-        "name" => "fantastic_split_with_information is off",
-        "description" => "This feature makes nothing happen."
-      )
+        expect(response_json).to include(
+          "name" => split_with_details.name,
+          "hypothesis" => split_with_details.hypothesis,
+          "location" => split_with_details.location,
+          "assignment_criteria" => split_with_details.assignment_criteria,
+          "platform" => split_with_details.platform,
+          "description" => split_with_details.description,
+          "owner" => split_with_details.owner
+        )
 
-      expect(response_json['variant_details'][1]['screenshot_url']).to include 'screenshot.jpg'
+        expect(response_json['variant_details'][0]).to eq(
+          "name" => "fantastic_split_with_information is on",
+          "description" => 'This awesome feature makes cool stuff happen.'
+        )
+
+        expect(response_json['variant_details'][1]).to include(
+          "name" => "fantastic_split_with_information is off",
+          "description" => "This feature makes nothing happen."
+        )
+
+        expect(response_json['variant_details'][1]['screenshot_url']).to include 'ttlogo.png'
+      end
     end
 
     it "blows up if split id is incorrect" do
