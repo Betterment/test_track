fab9c2ee04fb938e6600fecbb34ca6c788917045
agirlnamedsophia <sophia.russell@betterment.com>
2017-05-08 13:10:39 -0400

    Increases visibility around retiring variants and deciding splits; Modifies language on split show page to emphasize SPLIT over TEST

--- spec/support/pages/admin/split_show_page.rb
+++ spec/support/pages/admin/split_show_page.rb
@@ -4,12 +4,11 @@ class AdminSplitShowPage < SitePrism::Page
   element :population_count, "tr.population-row span.population"
 
   element :change_weights, ".change-weights-link"
-  element :retire_variant, ".retire-variant-link"
   element :add_details, ".add-details-link"
   element :decide_split, ".decide-split-link"
   element :upload_new_assignments, ".upload-new-assignments-link"
 
-  section :test_overview, ".TestOverview" do
+  section :test_overview, ".fs-SplitOverview" do
     element :table, ".DescriptionTable"
   end
 
@@ -19,5 +18,8 @@ class AdminSplitShowPage < SitePrism::Page
     element :description, 'td:nth-of-type(2)'
     element :weight, 'td:nth-of-type(3)'
     element :edit_link, 'td:nth-of-type(4) a'
+  end
+  def edit_variant(text)
+    variants_table.find('tr', text: text).find('a').click
   end
 end
--- spec/support/pages/admin/variant_details_edit_page.rb
+++ spec/support/pages/admin/variant_details_edit_page.rb
@@ -6,6 +6,11 @@ class AdminVariantDetailsEditPage < SitePrism::Page
     element :description, 'textarea[name="variant_detail[description]"]'
     element :screenshot, 'input[name="variant_detail[screenshot]"]'
     element :current_screenshot, '.variant_detail_screenshot .hint'
+    element :retire_button, '.retire-variant-link'
     element :submit_button, 'input[type=submit]'
+  end
+
+  def retire_variant
+    form.retire_button.click
   end
 end
