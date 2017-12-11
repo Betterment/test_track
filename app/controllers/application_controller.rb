5647daf1a3102944319496c548446e60cb28e293
Nathan Griffith <nathan.griffith@betterment.com>
2017-12-11 13:16:30 -0500

    protect_from_forgery no longer prepended by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+
   def after_sign_out_path_for(resource_name)
     if resource_name == :admin
       admin_root_path
