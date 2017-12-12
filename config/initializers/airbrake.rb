1e3f3a0dafd60d38ad58a4024f310c1ee4209cec
Nathan Griffith <nathan.griffith@betterment.com>
2017-12-12 11:18:39 -0500

    Keep airbrake where it was, monkey patch around UnfilteredParams

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -6,3 +6,21 @@ if ENV['AIRBRAKE_API_KEY'].present?
     config.secure  = config.port == 443
   end
 end
+
+module RailsFiveAirbrakeWorkaround
+  # Monkey-patching to work around usage of `ActionController::Parameters#to_hash`
+  # https://github.com/airbrake/airbrake/blob/v4.1.0/lib/airbrake/rails/controller_methods.rb#L5
+  def airbrake_request_data
+    {
+      parameters: airbrake_filter_if_filtering(params.to_unsafe_hash),
+      session_data: airbrake_filter_if_filtering(airbrake_session_data),
+      controller: params[:controller],
+      action: params[:action],
+      url: airbrake_request_url,
+      cgi_data: airbrake_filter_if_filtering(request.env),
+      user: airbrake_current_user
+    }
+  end
+end
+
+ActionController::Base.prepend RailsFiveAirbrakeWorkaround
