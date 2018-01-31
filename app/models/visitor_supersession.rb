76a7dc2cd82896c10d32173b97d42d153446d435
Sam Moore <sam@betterment.com>
2018-01-31 12:32:42 -0500

    address race condition in visitor supersession
    we occasionally see two supersessions occur concurrently for the same
    visitor. one of those will succeed and the other will fail. this results
    in the loss of the supersession history. this is not really a huge deal,
    but the annoying thing is that this results in an inactionable error.
    
    the solution in this changeset is to use savepoints and rescuing to
    swallow the unique violations.

--- app/models/visitor_supersession.rb
+++ app/models/visitor_supersession.rb
@@ -6,15 +6,24 @@ class VisitorSupersession < ActiveRecord::Base
 
   private
 
-  def merge_assignments! # rubocop:disable Metrics/AbcSize
+  def merge_assignments!
     target_split_ids = superseding_visitor.assignments.map(&:split_id).to_set
-    superseded_visitor.assignments.order(:id).map do |a|
+    superseded_visitor.assignments.order(:id).each do |a|
+      create_or_ignore_duplicate(a) unless target_split_ids.include?(a.split_id)
+    end
+  end
+
+  def create_or_ignore_duplicate(assignment)
+    transaction(requires_new: true) do
       superseding_visitor.assignments.create!(
-        variant: a.variant,
-        split_id: a.split_id,
+        variant: assignment.variant,
+        split_id: assignment.split_id,
         context: 'visitor_supersession',
         visitor_supersession: self
-      ) unless target_split_ids.include?(a.split_id)
+      )
     end
+  rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation # rubocop:disable Lint/HandleExceptions
+    # ignore or find
+    # we're not using the result, so probably just ignore
   end
 end
