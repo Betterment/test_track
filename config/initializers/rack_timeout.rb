9865be4579cc3b3064da2c577101ec38ede537c6
Sam Moore <samandmoore@gmail.com>
2018-02-22 16:45:37 -0500

    don't log rack-timeout things except errors, cut down max service time (#65)

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,11 @@
-Rack::Timeout.timeout = 120 # seconds
+Rack::Timeout.service_timeout = 30
+
+if Rails.env.development? || Rails.env.test?
+  Rack::Timeout::Logger.disable
+else
+  # have to clone the logger b/c https://github.com/heroku/rack-timeout/issues/104
+  Rails.logger.clone.tap do |l|
+    l.level = ::Logger::ERROR
+    Rack::Timeout::Logger.logger = l
+  end
+end
