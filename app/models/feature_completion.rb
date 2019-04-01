e2e5a152530e9015cf1e08fc9ca18110a7d38384
John Mileham <jmileham@gmail.com>
2019-04-01 17:04:50 -0400

    Update app/models/feature_completion.rb

--- app/models/feature_completion.rb
+++ app/models/feature_completion.rb
@@ -2,7 +2,8 @@ class FeatureCompletion < ActiveRecord::Base
   belongs_to :split
   belongs_to :app
 
-  validates :split_id, uniqueness: { scope: :app_id }
+  validates :split, uniqueness: { scope: :app }
+  validates :split, :app, :version, presence: true
 
   attribute :version, :app_version
 end
