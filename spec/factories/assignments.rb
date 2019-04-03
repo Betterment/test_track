3c334ed4640ccdb903251fc723e1985e0ec991eb
John Mileham <john@betterment.com>
2019-04-03 14:12:51 -0400

    Save rubocop from itself

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :assignment do
     visitor
     split
-    context { 'default_context' }
+    # rubocop misinterprets below as an RSpec context:
+    context { 'default_context' } # rubocop:disable RSpec/EmptyExampleGroup, RSpec/MissingExampleGroupArgument
     after(:build) { |assignment| assignment.variant ||= assignment.split.variants.first }
   end
 end
