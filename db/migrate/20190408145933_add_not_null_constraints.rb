7f791895226374e3f0bd4c35f358e494989274bf
John Mileham <john@betterment.com>
2019-04-11 15:40:06 -0400

    Drop constraints in down

--- db/migrate/20190408145933_add_not_null_constraints.rb
+++ db/migrate/20190408145933_add_not_null_constraints.rb
@@ -6,8 +6,8 @@ class AddNotNullConstraints < ActiveRecord::Migration[5.1]
     execute "ALTER TABLE bulk_assignments ADD CONSTRAINT bulk_assignments_force_check CHECK (force IS NOT NULL) NOT VALID;"
   end
   def down
-    execute "ALTER TABLE assignments ADD CONSTRAINT assignments_force_check;"
-    execute "ALTER TABLE previous_assignments ADD CONSTRAINT previous_assignments_force_check;"
-    execute "ALTER TABLE bulk_assignments ADD CONSTRAINT bulk_assignments_force_check;"
+    execute "ALTER TABLE assignments DROP CONSTRAINT assignments_force_check;"
+    execute "ALTER TABLE previous_assignments DROP CONSTRAINT previous_assignments_force_check;"
+    execute "ALTER TABLE bulk_assignments DROP CONSTRAINT bulk_assignments_force_check;"
   end
 end
