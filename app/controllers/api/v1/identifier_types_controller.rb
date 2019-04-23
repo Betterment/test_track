414d6a88a654bda1efde07c9ff4b8c3fe13f226a
John Mileham <john@betterment.com>
2019-04-23 15:18:31 -0400

    Don't allow cross-app confusion in identifier type creation

--- app/controllers/api/v1/identifier_types_controller.rb
+++ app/controllers/api/v1/identifier_types_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IdentifierTypesController < AuthenticatedApiController
   def create
-    identifier_type = IdentifierType.create_with(owner_app: current_app).find_or_initialize_by(create_params)
+    identifier_type = IdentifierType.find_or_initialize_by(create_params.merge(owner_app: current_app))
     if identifier_type.save
       head :no_content
     else
