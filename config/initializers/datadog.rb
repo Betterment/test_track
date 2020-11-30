f95908f229eb7779096b6c9534ded035a47270c4
Kelvin Acosta <kelvin.acosta@betterment.com>
2020-11-30 11:34:39 -0500

    update datadog config

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -5,10 +5,25 @@ if ENV['DATADOG_ENABLED']
     service_name = Rails.application.class.parent_name.underscore
 
     Datadog.configure do |c|
-      c.use :rails, service_name: service_name, distributed_tracing: true
+      c.env = Rails.env
+
+      c.tracer.hostname = ENV.fetch('DD_AGENT_HOST', 'localhost')
+      c.tracer.port = ENV.fetch('DD_TRACE_AGENT_PORT', 8126)
+
+      c.use :rails, service_name: service_name,
+                    distributed_tracing: true
+
+      c.use :rack, service_name: service_name,
+                   distributed_tracing: true,
+                   analytics_enabled: true
+
       c.use :active_record, orm_service_name: "#{service_name}-active_record"
+
       c.use :delayed_job, service_name: "#{service_name}-delayed_job"
-      c.use :http, service_name: "#{service_name}-http", distributed_tracing: true
+
+      c.use :http, service_name: "#{service_name}-http",
+                   distributed_tracing: true,
+                   analytics_enabled: true
     end
   end
 end
