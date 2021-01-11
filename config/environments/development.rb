451776a17bd7e85ba61cf463c975092f063cc372
Sam Moore <sam@betterment.com>
2021-01-11 14:14:45 -0500

    remove labs/demo environments and remove tagged logger wrapper
    these environments are no longer used internally at BMT and are not the
    preferred way of deploying apps on heroku, etc.
    
    the tagged logging wrapper is no longer necessary on rails 5.2+ / it
    causes a issue with calling silence=. the app works fine without it and
    is properly tagging logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,8 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 end
 
 ENV['WHITELIST_CORS_HOSTS'] ||= %w(.dev).join(',')
