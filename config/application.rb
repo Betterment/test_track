8be015b1a49d21fc550c3bea4143d1bb23ffad5a
HipsterBrown <headhipster@hipsterbrown.com>
2023-03-07 11:05:47 -0500

    chore: move primer setup in application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'view_component'
+require 'primer/view_components/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -28,8 +29,6 @@ module TestTrack
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    require 'view_component'
-    require 'primer/view_components/engine'
 
     config.load_defaults 6.0
 
