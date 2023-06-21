1c30b61270c134d3bce59907dcbc1fc19d59cd80
Ricardo Garza <ricardo.garza@betterment.com>
2023-06-21 09:14:10 -0600

    fix incorrect changes in env files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = ActiveRecord::Type::Boolean.new.cast(ENV.fetch('CACHE_CLASSES', '0'))
+  config.cache_classes = ActiveRecord::Type::Boolean.new.cast(ENV.fetch('CACHE_CLASSES', '1'))
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
