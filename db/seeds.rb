f0ca30431f55ee1519ffb3fa7328bd2622602915
Ricardo Vela <ricardo.garza@betterment.com>
2023-09-28 12:05:19 -0600

    get be rspec passing, .exists? -> .exist?

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 if Rails.env.development?
   seed_apps_filename = Rails.root.join('db/seed_apps.yml')
-  if File.exists?(seed_apps_filename)
+  if File.exist?(seed_apps_filename)
     YAML.load_file(seed_apps_filename).each do |app_name, auth_secret|
       App.create_with(auth_secret: auth_secret).find_or_create_by!(name: app_name).update!(auth_secret: auth_secret)
       puts "Ensured #{app_name} app"
