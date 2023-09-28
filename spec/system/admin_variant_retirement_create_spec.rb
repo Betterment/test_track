83291746c6a8b77db92b62a315432aa43388b312
Ricardo Vela <ricardo.garza@betterment.com>
2023-09-28 13:44:38 -0600

    yadpr (yet another dummy pr)

--- spec/system/admin_variant_retirement_create_spec.rb
+++ spec/system/admin_variant_retirement_create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'admin can retire a variant for a split' do
   let(:split_page) { app.admin_split_show_page }
-  let(:variant_page) { app.admin_variant_details_edit_page }
+  let!(:variant_page) { app.admin_variant_details_edit_page }
   let!(:split) { FactoryBot.create(:split, registry: { red: 25, blue: 0, green: 25, yellow: 25, orange: 25 }) }
 
   before do
