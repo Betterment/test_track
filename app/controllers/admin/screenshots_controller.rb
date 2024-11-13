b23a8f29e63497efb9bd4133c333bb5768688b9a
smudge <nathan@ngriffith.com>
2024-11-13 11:40:07 -0500

    New endpoint for viewing variant screenshots

---
+++ app/controllers/admin/screenshots_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+class Admin::ScreenshotsController < ApplicationController
+  def show
+    split = Split.find(params[:split_id])
+    variant_detail = split.variant_details.find_by!(variant: variant)
+    raise ActiveRecord::RecordNotFound unless variant_detail.screenshot_file_name?
+
+    redirect_to variant_detail.screenshot.expiring_url(300)
+  end
+
+  private
+
+  def variant
+    params.fetch(:id).split('.').first
+  end
+end
