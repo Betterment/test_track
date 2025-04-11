208d4027b385dc09a35a591a3cc3fbe5253a5e5c
Steve Jackson <steve.jackson@betterment.com>
2025-04-11 16:57:26 -0500

    Remove order by on assignments
    The value of the sort is questionable
    - when the dataset is small you don't need it
    - when the dataset is huge it raises the query cost
    - when its several dozens of assignments you might see the value
    
    A future version could sort the rows after the query, but that could
    also be misleading on huge datasets.

--- app/controllers/admin/split_assignments_controller.rb
+++ app/controllers/admin/split_assignments_controller.rb
@@ -4,7 +4,7 @@ class Admin::SplitAssignmentsController < AuthenticatedAdminController
   def index
     split = Split.find(params.require(:split_id))
     @split_name = split.name
-    @assignments = split.assignments.includes(visitor: { identifiers: :identifier_type }).order(updated_at: :desc).limit(VIEW_LIMIT)
+    @assignments = split.assignments.includes(visitor: { identifiers: :identifier_type }).limit(VIEW_LIMIT)
     flash.now[:warning] = "Only showing #{VIEW_LIMIT} of #{split.assignments.count} assignments" if split.assignments.count > VIEW_LIMIT
   end
 end
