aaf6523b3f717e3e8d16a97911bbe29c28337f20
Ricardo Vela <ricardo.garza@betterment.com>
2025-08-27 12:41:52 -0600

    rubocop -a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
+  config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
